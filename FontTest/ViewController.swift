c3118942e68a610a01cff0405167b829ce12fe27
vies <vies@list.ru>
2016-03-07 02:05:17 +0500

    Initial Commit

---
+++ FontTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FontTest
+//
+//  Created by ViES on 07.03.16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
